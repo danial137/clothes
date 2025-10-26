b07d2973e9d4cab003603af13f63129c8692006f
danial137 <danial79fakhrabadi@gmail.com>
2025-10-26 18:30:18 +0330

    add components

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
+import Container from "@/components/Container";
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <div>
-      <h2> persian clothes website</h2>
-      <Button variant="default">Demo Button</Button>
+      <Container>
+        <h2> persian clothes website</h2>
+        <Button variant="default">Demo Button</Button>
+      </Container>
     </div>
   );
 }
