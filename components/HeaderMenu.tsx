67f92512473b18bf4e80e6670d269b4dd7d38d1f
danial137 <danial79fakhrabadi@gmail.com>
2025-10-26 12:24:11 +0330

    add hover to navbar

--- components/HeaderMenu.tsx
+++ components/HeaderMenu.tsx
@@ -1,8 +1,14 @@
+import { headerData } from '@/constants'
+import Link from 'next/link'
 import React from 'react'
 
 const HeaderMenu = () => {
   return (
-    <div>HeaderMenu</div>
+    <div className='hidden md:inline-flex w-1/3 items-center gap-5 text-sm capitalize font-semibold'>{headerData?.map((item) => (
+      <Link key={item?.title} href={item?.href} className='hover:text-darkColor hoverEffect relative group'>{item?.title}
+        <span className='absolute -bottom-0.5 left-1/2 w-0 h-0.5 bg-darkColor hoverEffect group-hover:w-1/2 group-hover:left-0'></span>
+      </Link>
+    ))}</div>
   )
 }
 
