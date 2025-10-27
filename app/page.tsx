075e5c9c822c964fdf1fafe7545a6aa812a60231
danial137 <danial79fakhrabadi@gmail.com>
2025-10-27 19:50:58 +0330

    add  auth service

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
 import Container from "@/components/Container";
+import HomeBanner from "@/components/HomeBanner";
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <div>
-      <Container>
-        <h2> persian clothes website</h2>
-        <Button variant="default">Demo Button</Button>
+      <Container className="py-10">
+        <HomeBanner />
       </Container>
     </div>
   );
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,9 @@
+import CartIcon from "./CartIcon"
 import Container from "./Container"
 import HeaderMenu from "./HeaderMenu"
 import Logo from "./Logo"
+import MobileMenu from "./MobileMenu"
+import SearchBar from "./SearchBar"
 
 
 const Header = () => {
@@ -9,9 +12,18 @@ const Header = () => {
     <header className="border-b border-b-gray-400 py-5">
       <Container className="flex items-center justify-between gap-7 text-lightColor">
         <HeaderMenu />
-        <Logo />
-        <div>
-          right
+        <div className="w-auto md:w-1/3 flex items-center justify-center gap-2.5">
+          <MobileMenu />
+          <Logo>
+            TULOS
+          </Logo>
+        </div>
+        <div className="w-auto md:w-1/3 flex items-center  justify-end gap-5">
+          <SearchBar />
+          <CartIcon />
+          <div>
+            <button className="text-sm font-semibold hover:text-darkColor hoverEffect">Login</button>
+          </div>
         </div>
       </Container>
     </header>
