33413a9f45ea84fe8919e0fa55e6756b2ac6902b
danial137 <danial79fakhrabadi@gmail.com>
2025-11-13 17:51:20 +0330

    add search bar to project

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,7 +1,8 @@
 "use client"
-import { Search } from 'lucide-react'
+import { Search, X } from 'lucide-react'
 import React, { useState } from 'react'
 import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from './ui/dialog'
+import { Input } from './ui/input'
 
 const SearchBar = () => {
     const [search, setSearch] = useState('');
@@ -18,7 +19,13 @@ const SearchBar = () => {
                     <DialogTitle className='mb-1'>
                         Product SearchBar
                     </DialogTitle>
-                    <form className='relative'></form>
+                    <form className='relative' onSubmit={(e) => e.preventDefault()}>
+                        <Input placeholder='search your product here .... ' className='flex-1 rounded-md py-5' value={search} onChange={(e) => setSearch(e.target.value)} />
+                        {search && <X className='w-4 h-4 absolute top-3 right-11 hover:text-red-600 hoverEffect' />}
+                        <button type='submit' className='absolute right-0 top-0 bg-darkColor/10 w-10 h-full flex items-center justify-center rounded-tr-md rounded-br-md hover:bg-darkColor hover:text-white hoverEffect'>
+                            <Search className='w-5 h-5' />
+                        </button>
+                    </form>
                 </DialogHeader>
             </DialogContent>
         </Dialog>
