7ac09f2da2e7ff04b0359c7ad96a94d6539c56a4
danial137 <danial79fakhrabadi@gmail.com>
2025-11-25 14:54:59 +0330

    finish the demo version

--- sanity/helper/queries.ts
+++ sanity/helper/queries.ts
@@ -3,9 +3,8 @@ import { sanityFetch } from "../lib/live";
 
 export const getProductBySlug = async (slug: string) => {
   const PRODUCT_BY_SLUG_QUERY = defineQuery(
-    `*[_type == "product" && slug.current == $slug] | order(name asc)[0]`
+    `*[_type == 'product' && slug.current == $slug] | order(name asc) [0]`
   );
-
   try {
     const product = await sanityFetch({
       query: PRODUCT_BY_SLUG_QUERY,
@@ -13,24 +12,47 @@ export const getProductBySlug = async (slug: string) => {
         slug,
       },
     });
-
     return product?.data || null;
   } catch (error) {
-    console.error("error fetching product by slug", error);
+    console.error("Error fetching product by Slug:", error);
   }
 };
 
 export const getAllCategories = async () => {
   const CATEGORIES_QUERY = defineQuery(
-    `*[_type=='category'] | order(name asc) `
+    `*[_type=="category"] | order(name asc)`
   );
   try {
     const categories = await sanityFetch({
-      query:CATEGORIES_QUERY
+      query: CATEGORIES_QUERY,
     });
-    return categories.data || []
+    return categories.data || [];
   } catch (error) {
-    console.error("error fetchig product");
-    return []
+    console.error("Error fetching all categories");
+
+    return [];
+  }
+};
+
+export const getMyOrders = async (userId: string) => {
+  if (!userId) {
+    throw new Error("User ID is required");
+  }
+  const MY_ORDERS_QUERY =
+    defineQuery(`*[_type == 'order' && clerkUserId == $userId] | order(orderData desc){
+    ...,products[]{
+      ...,product->
+    }
+  }`);
+
+  try {
+    const orders = await sanityFetch({
+      query: MY_ORDERS_QUERY,
+      params: { userId },
+    });
+    return orders?.data || [];
+  } catch (error) {
+    console.error("Error fetching orders:", error);
+    return [];
   }
 };
