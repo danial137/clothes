ab6798ba53b33c858ada3cb3ec234e88b61a55e2
danial137 <danial79fakhrabadi@gmail.com>
2025-11-25 13:47:34 +0330

    add routes

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -1,5 +1,30 @@
-import { NextRequest } from "next/server";
+import { error } from "console";
+import { headers } from "next/headers";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest) {
   const body = await req.text();
+  const headersList = await headers();
+  const sig = headersList.get("stripe-signature");
+  if (!sig) {
+    return NextResponse.json(
+      {
+        error: "No Signature",
+      },
+      {
+        status: 400,
+      }
+    );
+  }
+
+  const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET;
+  if (!webhookSecret) {
+    console.log("stripe webhook secret is not set");
+    return NextResponse.json(
+      {
+        error: "stripe webhook secret is not set",
+      },
+      { status: 400 }
+    );
+  }
 }
